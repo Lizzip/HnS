833ab821527985bdf949614eb76796df2e57d9b5
Lizzip <lml4999@hotmail.com>
2013-06-23 17:36:23 +0100

    I am not a tit

--- HnS/HnS/HnS/Game1.cs
+++ HnS/HnS/HnS/Game1.cs
@@ -9,8 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-
-// You are a tit
 namespace HnS
 {
     public class Game1 : Microsoft.Xna.Framework.Game
